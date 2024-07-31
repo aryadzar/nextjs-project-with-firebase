4a83c104cec4f05ae35b33cbdec055b4dc7629c6
aryadzar <112409058+aryadzar@users.noreply.github.com>
2024-07-31 19:05:06 +0700

    progress

---
+++ src/app/product/[[...slug]]/loading.tsx
@@ -0,0 +1,36 @@
+export default function Loading(){
+    return (
+        <div>
+            <div className="grid grid-cols-4 mt-5 place-items-center">
+                <div className="w-full max-w-sm bg-white border  rounded-lg shadow dark:bg-gray-800 h-96 my-5">
+                    <div className="px-5 pb-5">
+                        <a href="#">
+                            <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white"></h5>
+                        </a>
+                    </div>
+                </div>
+                <div className="w-full max-w-sm bg-white border  rounded-lg shadow dark:bg-gray-800 h-96 my-5">
+                    <div className="px-5 pb-5">
+                        <a href="#">
+                            <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white"></h5>
+                        </a>
+                    </div>
+                </div>
+                <div className="w-full max-w-sm bg-white border  rounded-lg shadow dark:bg-gray-800 h-96 my-5">
+                    <div className="px-5 pb-5">
+                        <a href="#">
+                            <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white"></h5>
+                        </a>
+                    </div>
+                </div>
+                <div className="w-full max-w-sm bg-white border  rounded-lg shadow dark:bg-gray-800 h-96 my-5">
+                    <div className="px-5 pb-5">
+                        <a href="#">
+                            <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white"></h5>
+                        </a>
+                    </div>
+                </div>
+            </div>        
+        </div>
+    )
+}
